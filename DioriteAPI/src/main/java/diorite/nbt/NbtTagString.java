fc9874e7d4a1f26cff74c1dc0d568374fe443574
GotoFinal <bartlomiejkmazur@gmail.com>
2015-02-26 23:28:34 +0100

    Add NBT, untested

---
+++ DioriteAPI/src/main/java/diorite/nbt/NbtTagString.java
@@ -0,0 +1,74 @@
+package diorite.nbt;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class NbtTagString extends NbtAbstractTag<NbtTagString>
+{
+    protected String value;
+
+    public NbtTagString()
+    {
+    }
+
+    public NbtTagString(final String name)
+    {
+        super(name);
+    }
+
+    public NbtTagString(final String name, final String value)
+    {
+        super(name);
+        this.value = value;
+    }
+
+    public NbtTagString(final String name, final NbtTagCompound parent, final String value)
+    {
+        super(name, parent);
+        this.value = value;
+    }
+
+    @Override
+    public NbtTagString read(final NbtInputStream inputStream, final boolean hasName) throws IOException
+    {
+        super.read(inputStream, hasName);
+        final byte[] data = new byte[inputStream.readShort()];
+        inputStream.readFully(data);
+        this.value = new String(data, CHARSET);
+        return this;
+    }
+
+    @Override
+    public NbtTagString write(final NbtOutputStream outputStream, final boolean hasName) throws IOException
+    {
+        super.write(outputStream, hasName);
+        final byte[] bytes = this.value.getBytes(CHARSET);
+        outputStream.writeShort(bytes.length);
+        outputStream.write(bytes);
+        return this;
+    }
+
+    @Override
+    public NbtTagType getType()
+    {
+        return NbtTagType.STRING;
+    }
+
+    public String getValue()
+    {
+        return this.value;
+    }
+
+    public void setValue(final String value)
+    {
+        this.value = value;
+    }
+
+    @Override
+    public String toString()
+    {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).appendSuper(super.toString()).append("value", this.value).toString();
+    }
+}
---
+++ DioriteAPI/src/main/java/diorite/nbt/NbtTagType.java
@@ -0,0 +1,91 @@
+package diorite.nbt;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+import com.google.common.collect.ImmutableMap;
+
+@SuppressWarnings({"MagicNumber", "rawtypes"})
+public enum NbtTagType
+{
+    END(0, null, () -> null),
+    BYTE(1, NbtTagByte.class, NbtTagByte::new),
+    SHORT(2, NbtTagShort.class, NbtTagShort::new),
+    INTEGER(3, NbtTagInt.class, NbtTagInt::new),
+    LONG(4, NbtTagLong.class, NbtTagLong::new),
+    FLOAT(5, NbtTagFloat.class, NbtTagFloat::new),
+    DOUBLE(6, NbtTagDouble.class, NbtTagDouble::new),
+    BYTE_ARRAY(7, NbtTagByteArray.class, NbtTagString::new),
+    STRING(8, NbtTagString.class, NbtTagString::new),
+    LIST(9, NbtTagList.class, NbtTagList::new),
+    COMPOUND(10, NbtTagCompound.class, NbtTagCompound::new),
+    INTEGER_ARRAY(11, NbtTagIntArray.class, NbtTagIntArray::new);
+    private final        byte                                             typeID;
+    private final        Class<? extends NbtAbstractTag>                  typeClass;
+    private final        Supplier<NbtAbstractTag<?>>                      getInstance;
+    private static final Map<Byte, NbtTagType>                            typeMap;
+    private static final Map<Class<? extends NbtAbstractTag>, NbtTagType> classMap;
+
+    static
+    {
+        {
+            final ImmutableMap.Builder<Byte, NbtTagType> mapBuilder = new ImmutableMap.Builder<>();
+            for (final NbtTagType type : values())
+            {
+                mapBuilder.put(type.typeID, type);
+            }
+            typeMap = mapBuilder.build();
+        }
+        {
+            final ImmutableMap.Builder<Class<? extends NbtAbstractTag>, NbtTagType> mapBuilder = new ImmutableMap.Builder<>();
+            for (final NbtTagType type : values())
+            {
+                mapBuilder.put(type.typeClass, type);
+            }
+            classMap = mapBuilder.build();
+        }
+    }
+
+    private NbtTagType(final int typeID, final Class<? extends NbtAbstractTag> type, final Supplier<NbtAbstractTag<?>> getInstance)
+    {
+        this.typeID = ((byte) typeID);
+        this.typeClass = type;
+        this.getInstance = getInstance;
+    }
+
+    public byte getTypeID()
+    {
+        return this.typeID;
+    }
+
+    public Class<? extends NbtAbstractTag> getTypeClass()
+    {
+        return this.typeClass;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends NbtAbstractTag<?>> T newInstance()
+    {
+        return (T) this.getInstance.get();
+    }
+
+    public static Map<Byte, NbtTagType> getTypeMap()
+    {
+        return typeMap;
+    }
+
+    public static Map<Class<? extends NbtAbstractTag>, NbtTagType> getClassMap()
+    {
+        return classMap;
+    }
+
+    public static NbtTagType valueOf(final byte typeID)
+    {
+        return typeMap.get(typeID);
+    }
+
+    public static NbtTagType valueOf(final Class<?> clazz)
+    {
+        return classMap.get(clazz);
+    }
+}
