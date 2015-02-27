1a92d8fb0d62d877621466163aac6aa334d51f78
GotoFinal <bartlomiejkmazur@gmail.com>
2015-02-27 20:26:13 +0100

    Playing around chunks, moved ChunkPos to API

---
+++ DioriteAPI/src/main/java/diorite/Location.java
@@ -0,0 +1,20 @@
+package diorite;
+
+import diorite.map.chunk.ChunkPos;
+
+public interface Location
+{
+    int getBlockX();
+
+    int getBlockY();
+
+    int getBlockZ();
+
+    double getX();
+
+    double getY();
+
+    double getZ();
+
+    ChunkPos getChunkPos
+}
---
+++ DioriteAPI/src/main/java/diorite/map/chunk/ChunkPos.java
@@ -0,0 +1,58 @@
+package diorite.map.chunk;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class ChunkPos
+{
+    private final int x;
+    private final int z;
+
+    public ChunkPos(final int x, final int z)
+    {
+        this.x = x;
+        this.z = z;
+    }
+
+    public int getX()
+    {
+        return this.x;
+    }
+
+    public int getZ()
+    {
+        return this.z;
+    }
+
+    @Override
+    public boolean equals(final Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (! (o instanceof ChunkPos))
+        {
+            return false;
+        }
+
+        final ChunkPos chunkPos = (ChunkPos) o;
+
+        return (this.x == chunkPos.x) && (this.z == chunkPos.z);
+
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = this.x;
+        result = (31 * result) + this.z;
+        return result;
+    }
+
+    @Override
+    public String toString()
+    {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).appendSuper(super.toString()).append("x", this.x).append("z", this.z).toString();
+    }
+}
