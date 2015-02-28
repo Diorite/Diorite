22ad37f74d66d4859015586973cc85fc2d698ac9
GotoFinal <bartlomiejkmazur@gmail.com>
2015-02-28 22:03:25 +0100

    Still playing around chunks.

---
+++ DioriteAPI/src/main/java/diorite/map/chunk/Chunk.java
@@ -0,0 +1,9 @@
+package diorite.map.chunk;
+
+public interface Chunk
+{
+    int CHUNK_SIZE        = 16;
+    int CHUNK_PARTS       = 16;
+    int CHUNK_PART_HEIGHT = 16;
+    int CHUNK_FULL_HEIGHT = 256;
+}
