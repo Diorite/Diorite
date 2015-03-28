dc0996977bf26d1b211ca3f0dec88b00af43f19c
GotoFinal <bartlomiejkmazur@gmail.com>
2015-03-28 01:20:01 +0100

    better nicknames tab-complete, say,broadcast and online commands, disconnect handlers

--- DioriteAPI/src/main/java/org/diorite/Server.java
+++ DioriteAPI/src/main/java/org/diorite/Server.java
@@ -1,5 +1,7 @@
 package org.diorite;
 
+import org.diorite.chat.ChatPosition;
+import org.diorite.chat.component.BaseComponent;
 import org.diorite.command.CommandMap;
 import org.diorite.command.sender.ConsoleCommandSender;
 import org.diorite.command.PluginCommandBuilder;
@@ -19,6 +21,54 @@ public interface Server
     byte   DEFAULT_RENDER_DISTANCE = 10;
     int    DEFAULT_WAIT_TIME       = NANOS_IN_SECOND / DEFAULT_TPS;
     int    MAX_NICKNAME_SIZE       = 16;
+
+    void broadcastMessage(ChatPosition position, String str);
+
+    void broadcastMessage(ChatPosition position, String... strs);
+
+    void broadcastSimpleColoredMessage(ChatPosition position, String str);
+
+    void broadcastSimpleColoredMessage(ChatPosition position, String... strs);
+
+    void broadcastDioriteMessage(ChatPosition position, String str);
+
+    void broadcastDioriteMessage(ChatPosition position, String... strs);
+
+    void broadcastMessage(ChatPosition position, BaseComponent component);
+
+    void broadcastMessage(ChatPosition position, BaseComponent... components);
+
+    void broadcastMessage(String str);
+
+    void broadcastMessage(String... strs);
+
+    void broadcastSimpleColoredMessage(String str);
+
+    void broadcastSimpleColoredMessage(String... strs);
+
+    void broadcastDioriteMessage(String str);
+
+    void broadcastDioriteMessage(String... strs);
+
+    void broadcastMessage(BaseComponent component);
+
+    void broadcastMessage(BaseComponent... components);
+
+    void sendConsoleMessage(String str);
+
+    void sendConsoleMessage(String... strs);
+
+    void sendConsoleMessage(BaseComponent component);
+
+    void sendConsoleMessage(BaseComponent... components);
+
+    void sendConsoleSimpleColoredMessage(String str);
+
+    void sendConsoleSimpleColoredMessage(String... strs);
+
+    void sendConsoleDioriteMessage(String str);
+
+    void sendConsoleDioriteMessage(String... strs);
 
     byte getRenderDistance();
 
--- DioriteAPI/src/main/java/org/diorite/command/sender/PlayerCommandSender.java
+++ DioriteAPI/src/main/java/org/diorite/command/sender/PlayerCommandSender.java
@@ -1,5 +1,10 @@
 package org.diorite.command.sender;
 
+import org.diorite.chat.ChatColor;
+import org.diorite.chat.ChatPosition;
+import org.diorite.chat.DioriteMarkdownParser;
+import org.diorite.chat.component.BaseComponent;
+import org.diorite.chat.component.TextComponent;
 import org.diorite.entity.Player;
 
 public interface PlayerCommandSender extends CommandSender
@@ -23,4 +28,77 @@ public interface PlayerCommandSender extends CommandSender
     }
 
     Player getPlayer();
+
+    @Override
+    default void sendMessage(final String str)
+    {
+        this.sendMessage(new TextComponent(str));
+    }
+
+    default void sendMessage(final ChatPosition position, final String str)
+    {
+        this.sendMessage(position, new TextComponent(str));
+    }
+
+    default void sendMessage(final ChatPosition position, final String[] strs)
+    {
+        if (strs != null)
+        {
+            for (final String str : strs)
+            {
+                this.sendMessage(position, str);
+            }
+        }
+    }
+
+    default void sendSimpleColoredMessage(final ChatPosition position, final String str)
+    {
+        this.sendMessage(position, ChatColor.translateAlternateColorCodes(str));
+    }
+
+    default void sendSimpleColoredMessage(final ChatPosition position, final String[] strs)
+    {
+        if (strs != null)
+        {
+            for (final String str : strs)
+            {
+                this.sendSimpleColoredMessage(position, str);
+            }
+        }
+    }
+
+    default void sendDioriteMessage(final ChatPosition position, final String str)
+    {
+        this.sendMessage(position, DioriteMarkdownParser.parse(str, false));
+    }
+
+    default void sendDioriteMessage(final ChatPosition position, final String[] strs)
+    {
+        if (strs != null)
+        {
+            for (final String str : strs)
+            {
+                this.sendDioriteMessage(position, str);
+            }
+        }
+    }
+
+    @Override
+    default void sendMessage(final BaseComponent component)
+    {
+        this.sendMessage(ChatPosition.SYSTEM, component);
+    }
+
+    void sendMessage(ChatPosition position, BaseComponent component);
+
+    default void sendMessage(final ChatPosition position, final BaseComponent[] components)
+    {
+        if (components != null)
+        {
+            for (final BaseComponent component : components)
+            {
+                this.sendMessage(position, component);
+            }
+        }
+    }
 }
