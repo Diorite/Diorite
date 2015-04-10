d83d612594a9df39ced8eb3c6cf0f8aeb45db6c7
GotoFinal <bartlomiejkmazur@gmail.com>
2015-04-10 23:44:35 +0200

    playing around eq

--- DioriteAPI/src/main/java/org/diorite/inventory/Inventory.java
+++ DioriteAPI/src/main/java/org/diorite/inventory/Inventory.java
@@ -4,7 +4,13 @@ import org.diorite.inventory.item.ItemStack;
 
 public interface Inventory
 {
+    int remove(ItemStack itemStack);
+
+    boolean removeItem(ItemStack itemStack);
+
     boolean contains(ItemStack itemStack);
+
+    boolean containsAtLeast(ItemStack itemStack);
 
     boolean add(ItemStack... itemStack);
 }
