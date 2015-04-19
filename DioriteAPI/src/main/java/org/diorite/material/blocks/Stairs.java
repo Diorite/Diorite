ddc9a6213e255d8188a7b7a60aa10f5bf3a32861
GotoFinal <bartlomiejkmazur@gmail.com>
2015-04-19 14:51:51 +0200

    EndPortalFrame and started implementing staris, will not compile.

--- DioriteAPI/src/main/java/org/diorite/material/blocks/Stairs.java
+++ DioriteAPI/src/main/java/org/diorite/material/blocks/Stairs.java
@@ -1,5 +1,32 @@
 package org.diorite.material.blocks;
 
-public interface Stairs // TODO: implement
+import org.diorite.BlockFace;
+
+public interface Stairs extends Directional
 {
+    byte UPSIDE_DOWN_FLAG = 0x4;
+
+    void isUpsideDown(); // TODO: implement
+
+    void getUpsideDown(final boolean upsideDown);
+
+    static byte combine(final BlockFace face, final boolean upside)
+    {
+        byte value = upside ? UPSIDE_DOWN_FLAG : 0x0;
+        switch (face)
+        {
+            case WEST:
+                value += 0x1;
+                break;
+            case SOUTH:
+                value += 0x2;
+                break;
+            case NORTH:
+                value += 0x3;
+                break;
+            default:
+                break;
+        }
+        return value;
+    }
 }
--- DioriteAPI/src/main/java/org/diorite/material/blocks/end/EndPortalFrame.java
+++ DioriteAPI/src/main/java/org/diorite/material/blocks/end/EndPortalFrame.java
@@ -2,8 +2,14 @@ package org.diorite.material.blocks.end;
 
 import java.util.Map;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import org.diorite.BlockFace;
 import org.diorite.cfg.magic.MagicNumbers;
 import org.diorite.material.BlockMaterialData;
+import org.diorite.material.blocks.Activatable;
+import org.diorite.material.blocks.Directional;
 import org.diorite.utils.collections.SimpleStringHashMap;
 
 import gnu.trove.map.TByteObjectMap;
@@ -12,13 +18,16 @@ import gnu.trove.map.hash.TByteObjectHashMap;
 /**
  * Class representing block "EndPortalFrame" and all its subtypes.
  */
-public class EndPortalFrame extends BlockMaterialData
+public class EndPortalFrame extends BlockMaterialData implements Directional, Activatable
 {
-    // TODO: auto-generated class, implement other types (sub-ids).	
     /**
      * Sub-ids used by diorite/minecraft by default
      */
-    public static final byte  USED_DATA_VALUES = 1;
+    public static final byte  USED_DATA_VALUES = 8;
+    /**
+     * Flag that determine if this is activated sub-type
+     */
+    public static final byte  ACTIVATE_FLAG    = 0x4;
     /**
      * Blast resistance of block, can be changed only before server start.
      * Final copy of blast resistance from {@link MagicNumbers} class.
@@ -30,25 +39,54 @@ public class EndPortalFrame extends BlockMaterialData
      */
     public static final float HARDNESS         = MagicNumbers.MATERIAL__END_PORTAL_FRAME__HARDNESS;
 
-    public static final EndPortalFrame END_PORTAL_FRAME = new EndPortalFrame();
+    public static final EndPortalFrame END_PORTAL_FRAME_SOUTH     = new EndPortalFrame();
+    public static final EndPortalFrame END_PORTAL_FRAME_WEST      = new EndPortalFrame("WEST", BlockFace.WEST, false);
+    public static final EndPortalFrame END_PORTAL_FRAME_NORTH     = new EndPortalFrame("NORTH", BlockFace.NORTH, false);
+    public static final EndPortalFrame END_PORTAL_FRAME_EAST      = new EndPortalFrame("EAST", BlockFace.EAST, false);
+    public static final EndPortalFrame END_PORTAL_FRAME_SOUTH_EYE = new EndPortalFrame("SOUTH_EYE", BlockFace.SOUTH, true);
+    public static final EndPortalFrame END_PORTAL_FRAME_WEST_EYE  = new EndPortalFrame("WEST_EYE", BlockFace.WEST, true);
+    public static final EndPortalFrame END_PORTAL_FRAME_NORTH_EYE = new EndPortalFrame("NORTH_EYE", BlockFace.NORTH, true);
+    public static final EndPortalFrame END_PORTAL_FRAME_EAST_EYE  = new EndPortalFrame("EAST_EYE", BlockFace.EAST, true);
 
     private static final Map<String, EndPortalFrame>    byName = new SimpleStringHashMap<>(USED_DATA_VALUES, SMALL_LOAD_FACTOR);
     private static final TByteObjectMap<EndPortalFrame> byID   = new TByteObjectHashMap<>(USED_DATA_VALUES, SMALL_LOAD_FACTOR);
 
+    private final BlockFace face;
+    private final boolean   activated;
+
     @SuppressWarnings("MagicNumber")
     protected EndPortalFrame()
     {
-        super("END_PORTAL_FRAME", 120, "minecraft:end_portal_frame", "END_PORTAL_FRAME", (byte) 0x00);
+        super("END_PORTAL_FRAME", 120, "minecraft:end_portal_frame", "SOUTH", (byte) 0x00);
+        this.face = BlockFace.SOUTH;
+        this.activated = false;
     }
 
-    public EndPortalFrame(final String enumName, final int type)
+    public EndPortalFrame(final String enumName, final BlockFace face, final boolean activated)
     {
-        super(END_PORTAL_FRAME.name(), END_PORTAL_FRAME.getId(), END_PORTAL_FRAME.getMinecraftId(), enumName, (byte) type);
+        super(END_PORTAL_FRAME.name(), END_PORTAL_FRAME.getId(), END_PORTAL_FRAME.getMinecraftId(), enumName, combine(face, activated));
+        this.face = face;
+        this.activated = activated;
     }
 
-    public EndPortalFrame(final int maxStack, final String typeName, final byte type)
+    protected static byte combine(final BlockFace face, final boolean activated)
     {
-        super(END_PORTAL_FRAME.name(), END_PORTAL_FRAME.getId(), END_PORTAL_FRAME.getMinecraftId(), maxStack, typeName, type);
+        byte value = activated ? ACTIVATE_FLAG : 0x0;
+        switch (face)
+        {
+            case WEST:
+                value += 0x1;
+                break;
+            case NORTH:
+                value += 0x2;
+                break;
+            case EAST:
+                value += 0x3;
+                break;
+            default:
+                break;
+        }
+        return value;
     }
 
     @Override
@@ -73,6 +111,44 @@ public class EndPortalFrame extends BlockMaterialData
     public EndPortalFrame getType(final int id)
     {
         return getByID(id);
+    }
+
+    @Override
+    public BlockFace getBlockFacing()
+    {
+        return this.face;
+    }
+
+    @Override
+    public EndPortalFrame getBlockFacing(final BlockFace face)
+    {
+        return getByID(combine(face, this.activated));
+    }
+
+    @Override
+    public boolean isActivated()
+    {
+        return this.activated;
+    }
+
+    @Override
+    public EndPortalFrame getActivated(final boolean activated)
+    {
+        return getByID(combine(this.face, activated));
+    }
+
+    /**
+     * Returns one of EndPortalFrame sub-type based on {@link BlockFace} and activated status.
+     * It will never return null;
+     *
+     * @param face      face of block, unsupported face will cause using face from default type.
+     * @param activated if it have eye of ender in it.
+     *
+     * @return sub-type of EndPortalFrame
+     */
+    public EndPortalFrame getType(final BlockFace face, final boolean activated)
+    {
+        return getByID(combine(face, activated));
     }
 
     /**
@@ -101,6 +177,20 @@ public class EndPortalFrame extends BlockMaterialData
     }
 
     /**
+     * Returns one of EndPortalFrame sub-type based on {@link BlockFace} and activated status.
+     * It will never return null;
+     *
+     * @param face      face of block, unsupported face will cause using face from default type.
+     * @param activated if it have eye of ender in it.
+     *
+     * @return sub-type of EndPortalFrame
+     */
+    public static EndPortalFrame getEndPortalFrame(final BlockFace face, final boolean activated)
+    {
+        return getByID(combine(face, activated));
+    }
+
+    /**
      * Register new sub-type, may replace existing sub-types.
      * Should be used only if you know what are you doing, it will not create fully usable material.
      *
@@ -115,5 +205,11 @@ public class EndPortalFrame extends BlockMaterialData
     static
     {
         EndPortalFrame.register(END_PORTAL_FRAME);
+    }
+
+    @Override
+    public String toString()
+    {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).appendSuper(super.toString()).append("face", this.face).append("activated", this.activated).toString();
     }
 }
