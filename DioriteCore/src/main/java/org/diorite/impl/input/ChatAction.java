e132831a86e5c7ff4fe3896e99e791a0711b32e8
GotoFinal <bartlomiejkmazur@gmail.com>
2015-06-03 04:15:32 +0200

    merged input threads fix

---
+++ DioriteCore/src/main/java/org/diorite/impl/input/ChatAction.java
@@ -0,0 +1,73 @@
+package org.diorite.impl.input;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import org.diorite.command.sender.CommandSender;
+
+public class ChatAction
+{
+    private final String        msg;
+    private final CommandSender sender;
+    private final Type          type;
+
+    public ChatAction(final String msg, final CommandSender sender, final Type type)
+    {
+        this.msg = msg;
+        this.sender = sender;
+        this.type = type;
+    }
+
+    public String getMsg()
+    {
+        return this.msg;
+    }
+
+    public CommandSender getSender()
+    {
+        return this.sender;
+    }
+
+    public Type getType()
+    {
+        return this.type;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = (this.msg != null) ? this.msg.hashCode() : 0;
+        result = (31 * result) + ((this.sender != null) ? this.sender.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (! (o instanceof ChatAction))
+        {
+            return false;
+        }
+
+        final ChatAction that = (ChatAction) o;
+
+        return ! ((this.msg != null) ? ! this.msg.equals(that.msg) : (that.msg != null)) && ! ((this.sender != null) ? ! this.sender.equals(that.sender) : (that.sender != null));
+    }
+
+    @Override
+    public String toString()
+    {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).appendSuper(super.toString()).append("msg", this.msg).append("sender", this.sender).toString();
+    }
+
+    public enum Type
+    {
+        CHAT,
+        COMMAND,
+        TAB_COMPLETE;
+    }
+}
