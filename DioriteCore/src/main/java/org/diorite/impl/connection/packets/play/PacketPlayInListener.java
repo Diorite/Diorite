8e7c65316fb062e3391eefa96a657105ccec28c5
Michał <northpl93@gmail.com>
2015-07-10 14:50:35 +0200

    Code style fix

--- DioriteCore/src/main/java/org/diorite/impl/connection/packets/play/PacketPlayInListener.java
+++ DioriteCore/src/main/java/org/diorite/impl/connection/packets/play/PacketPlayInListener.java
@@ -1,7 +1,31 @@
 package org.diorite.impl.connection.packets.play;
 
 import org.diorite.impl.connection.packets.PacketListener;
-import org.diorite.impl.connection.packets.play.in.*;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInAbilities;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInArmAnimation;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInBlockDig;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInBlockPlace;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInChat;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInClientCommand;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInCloseWindow;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInCustomPayload;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInEnchantItem;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInEntityAction;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInFlying;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInHeldItemSlot;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInKeepAlive;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInLook;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInPosition;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInPositionLook;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInResourcePackStatus;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInSetCreativeSlot;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInSettings;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInSpectate;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInSteerVehicle;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInTabComplete;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInTransaction;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInUpdateSign;
+import org.diorite.impl.connection.packets.play.in.PacketPlayInWindowClick;
 
 public interface PacketPlayInListener extends PacketListener
 {
