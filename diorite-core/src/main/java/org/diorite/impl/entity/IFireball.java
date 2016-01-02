5ed039c4c8eeade2bf1eb158d2f63787ee802036
GotoFinal <bartlomiejkmazur@gmail.com>
2016-01-02 23:35:32 +0100

    ugh, windows

---
+++ diorite-core/src/main/java/org/diorite/impl/entity/IFireball.java
@@ -0,0 +1,9 @@
+package org.diorite.impl.entity;
+
+import org.diorite.entity.FireworksRocket;
+import org.diorite.utils.math.geometry.ImmutableEntityBoundingBox;
+
+public interface IFireball extends IProjectile, FireworksRocket
+{
+    ImmutableEntityBoundingBox BASE_SIZE = new ImmutableEntityBoundingBox(0.25F, 0.25F);
+}
