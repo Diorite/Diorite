5bf82adda3b9975516ebf1a5ec76af07905df7d2
mibac138 <mibac138@wp.pl>
2016-08-04 00:24:50 +0200

    Further trapped chest implementation

--- diorite-api/src/main/java/org/diorite/block/TrappedChest.java
+++ diorite-api/src/main/java/org/diorite/block/TrappedChest.java
@@ -1,8 +1,17 @@
 package org.diorite.block;
 
+import org.diorite.inventory.block.TrappedChestInventory;
+
 /**
  * Represents a trapped chest
  */
 public interface TrappedChest extends Chest
 {
+    /**
+     * Returns chest's inventory
+     *
+     * @return inventory
+     */
+    @Override
+    TrappedChestInventory getInventory();
 }
--- diorite-core/src/main/java/org/diorite/impl/block/ChestImpl.java
+++ diorite-core/src/main/java/org/diorite/impl/block/ChestImpl.java
@@ -1,22 +1,25 @@
 package org.diorite.impl.block;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 import org.diorite.impl.inventory.block.ChestInventoryImpl;
-import org.diorite.impl.tileentity.TileEntityChestImpl;
 import org.diorite.impl.world.WorldImpl;
 import org.diorite.block.Block;
 import org.diorite.block.Chest;
 import org.diorite.inventory.block.ChestInventory;
+import org.diorite.tileentity.TileEntityChest;
 
 public class ChestImpl extends BlockStateImpl implements Chest
 {
-    private final TileEntityChestImpl chest;
-    private final ChestInventory      inventory;
+    private final TileEntityChest chest;
+    private final ChestInventory  inventory;
 
     public ChestImpl(final Block block)
     {
         super(block);
 
-        this.chest = (TileEntityChestImpl) ((WorldImpl) block.getWorld()).getTileEntity(block.getLocation());
+        this.chest = (TileEntityChest) ((WorldImpl) block.getWorld()).getTileEntity(block.getLocation());
         this.inventory = new ChestInventoryImpl(this);
     }
 
@@ -24,5 +27,11 @@ public class ChestImpl extends BlockStateImpl implements Chest
     public ChestInventory getInventory()
     {
         return this.inventory;
+    }
+
+    @Override
+    public String toString()
+    {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).appendSuper(super.toString()).append("chest", this.chest).append("inventory", this.inventory).toString();
     }
 }
