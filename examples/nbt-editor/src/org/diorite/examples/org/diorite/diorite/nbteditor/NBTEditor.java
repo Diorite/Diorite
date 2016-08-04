ba27469a7effced8882ffdeff104391d080bb7cb
mibac138 <mibac138@wp.pl>
2016-08-04 13:32:42 +0200

    NBTEditor relocation

--- examples/nbt-editor/src/org/diorite/examples/org/diorite/diorite/nbteditor/NBTEditor.java
+++ examples/nbt-editor/src/org/diorite/examples/org/diorite/diorite/nbteditor/NBTEditor.java
@@ -1,20 +1,25 @@
-package org.diorite.mibac.nbteditor;
+package org.diorite.diorite.nbteditor;
 
-import static org.diorite.mibac.nbteditor.NBTEditor.AUTHOR;
-import static org.diorite.mibac.nbteditor.NBTEditor.NAME;
-import static org.diorite.mibac.nbteditor.NBTEditor.VERSION;
+import static org.diorite.diorite.nbteditor.NBTEditor.AUTHOR;
+import static org.diorite.diorite.nbteditor.NBTEditor.NAME;
+import static org.diorite.diorite.nbteditor.NBTEditor.VERSION;
+import static org.diorite.diorite.nbteditor.NBTEditor.WEBSITE;
 
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import org.diorite.cfg.messages.PluginMessages;
 import org.diorite.plugin.DioritePlugin;
 import org.diorite.plugin.Plugin;
 
-@Plugin(name = NAME, version = VERSION, author = AUTHOR)
+@Plugin(name = NAME, version = VERSION, author = AUTHOR, website = WEBSITE)
 public class NBTEditor extends DioritePlugin
 {
     public static final String NAME    = "NBTEditor";
     public static final String VERSION = "1.0";
     public static final String AUTHOR  = "mibac138";
+    public static final String WEBSITE = "diorite.org";
 
     private PluginMessages language;
     private NBTCommand     commands;
@@ -42,5 +47,11 @@ public class NBTEditor extends DioritePlugin
     private void registerCommands()
     {
         this.commands = new NBTCommand(this);
+    }
+
+    @Override
+    public String toString()
+    {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).appendSuper(super.toString()).append("language", this.language).append("commands", this.commands).toString();
     }
 }
