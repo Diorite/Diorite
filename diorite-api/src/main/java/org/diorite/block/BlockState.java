aeb47d935a177e3c59dda4fb8abb1d7677fda6b5
skprime <skprime@wp.pl>
2016-08-04 22:07:28 +0200

    Added/corrected javadocs

--- diorite-api/src/main/java/org/diorite/block/BlockState.java
+++ diorite-api/src/main/java/org/diorite/block/BlockState.java
@@ -27,25 +27,85 @@ package org.diorite.block;
 import org.diorite.material.BlockMaterialData;
 import org.diorite.world.World;
 
+/**
+ * Represents block state.
+ */
 public interface BlockState
 {
+    /**
+     * @return x coordinate of block in this block state in {@link World}.
+     */
     int getX();
+
+    /**
+     * @return y coordinate of block in this block state in {@link World}.
+     */
     int getY();
+
+    /**
+     * @return z coordinate of block in this block state in {@link World}.
+     */
     int getZ();
 
+    /**
+     * @return block of this block state.
+     */
     Block getBlock();
 
+    /**
+     * @return type (id and sub-id) of block in this block state.
+     */
     BlockMaterialData getType();
 
+    /**
+     * Method to set type (id and sub-id) of block in this block state.
+     * This method will only update local copy of it (this wrapper), block will be updated on map after calling {@link #update()} method.
+     *
+     * @param type new type (id and sub-id) of block in this block state.
+     */
     void setType(BlockMaterialData type);
 
+    /**
+     * @return {@link World} where block of this block state exist.
+     */
     World getWorld();
 
-    BlockLocation getLocation();
+    /**
+     * @return {@link BlockLocation} of block of this block state.
+     */
+    default BlockLocation getLocation()
+    {
+        return new BlockLocation(this.getX(), this.getY(), this.getZ(), this.getWorld());
+    }
 
+    /**
+     * Updates block of this block state.
+     *
+     * @return true if update was successful.
+     */
     boolean update();
+
+    /**
+     * Updates block of this block state.
+     *
+     * @param force to force update.
+     *
+     * @return true if update was successful.
+     */
     boolean update(boolean force);
+
+    /**
+     * Updates block of this block state.
+     *
+     * @param force to force update.
+     * @param applyPhysics to apply physics to the block.
+     *
+     * @return true if update was successful.
+     */
     boolean update(boolean force, boolean applyPhysics);
 
+    /**
+     * @return true if block of this block state if placed.
+     */
     boolean isPlaced();
 }
