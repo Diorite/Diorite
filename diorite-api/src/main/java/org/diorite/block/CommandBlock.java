24f1ef1adc3a3dfaa60315282cc79ea5b7b43fd1
skprime <skprime@wp.pl>
2016-08-04 21:59:05 +0200

    Added command block

---
+++ diorite-api/src/main/java/org/diorite/block/CommandBlock.java
@@ -0,0 +1,59 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016. Diorite (by Bartłomiej Mazur (aka GotoFinal))
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.diorite.block;
+
+public interface CommandBlock extends BlockState
+{
+    /**
+     * Gets the command from this command block that will execute
+     * when block will get powered.
+     * If the command block doesn't have a command, it will return an empty string.
+     *
+     * @return the command.
+     */
+    String getCommand();
+
+    /**
+     * Sets the command of this command block, that will be executed after
+     * this command block get powered.
+     *
+     * @param command is the command.
+     */
+    void setCommand(String command);
+
+    /**
+     * Gets the name of this command block.
+     *
+     * @return the name.
+     */
+    String getName();
+
+    /**
+     * Sets the name of this command block.
+     *
+     * @param name is the name.
+     */
+    void setName(String name);
+}
