9c2c52514749c519a9cc4038c5954125d7b0ce6f
GotoFinal <bartlomiejkmazur@gmail.com>
2016-12-17 22:50:59 +0100

    fixed exit code.

--- travis/testCopyright.sh
+++ travis/testCopyright.sh
@@ -4,8 +4,9 @@ cmd=$(grep -Lr --include "*.java" "The MIT License (MIT)" ../*);
 if [ -z "$cmd" ]
 then
     echo "[PASS] Found license headers in all files.";
+    exit 0;
 else
     echo "[FAIL] Missing license headers in: "
     echo "$cmd";
+    exit 1;
 fi
-exit 1;
