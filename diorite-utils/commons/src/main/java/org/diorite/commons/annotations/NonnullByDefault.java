e4857db8fcf5571704c621ca8c19bf091341d097
GotoFinal <bartlomiejkmazur@gmail.com>
2016-12-17 23:25:42 +0100

    Don't apply Nonnull on local variables.

--- diorite-utils/commons/src/main/java/org/diorite/commons/annotations/NonnullByDefault.java
+++ diorite-utils/commons/src/main/java/org/diorite/commons/annotations/NonnullByDefault.java
@@ -46,7 +46,6 @@ import java.lang.annotation.RetentionPolicy;
                 ElementType.ANNOTATION_TYPE,
                 ElementType.CONSTRUCTOR,
                 ElementType.FIELD,
-                ElementType.LOCAL_VARIABLE,
                 ElementType.METHOD,
                 ElementType.PACKAGE,
                 ElementType.PARAMETER,
