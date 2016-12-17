386013b1fbca57b0d79ff6e150af3a7d6ccc7d38
Bartłomiej Mazur <GotoFinal@users.noreply.github.com>
2016-12-17 22:53:02 +0100

    Add travis Copyright check. (#79)
    [skip ci]

---
+++ travis/testCopyright.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo "Testing for missing license headers..."
+cmd=$(grep -Lr --include "*.java" "The MIT License (MIT)" ../*);
+if [ -z "$cmd" ]
+then
+    echo "[PASS] Found license headers in all files.";
+    exit 0;
+else
+    echo "[FAIL] Missing license headers in: "
+    echo "$cmd";
+    exit 1;
+fi
